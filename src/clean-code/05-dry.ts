485a1c7da3013582aca96c21086f6235f110f3d6
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-17 14:18:45 -0600

    fix: Se aplica principio DRY en el ejercicio

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,11 +8,25 @@ class Product {
     ) {
     }
 
+    productIsValid(): boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if (this[key].length === 0) throw new Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if ((this[key]) <= 0) throw new Error(`${key} is zero or less than zero`);
+                    break
+                default:
+                    throw new Error(`${key} is not valid`);
+            }
+        }
+        return true;
+    }
+
     toString() {
-        // No Dry
-        if (this.name.length === 0) throw Error('Name is empty');
-        if (this.price <= 0) throw Error('Price is zero');
-        if (this.size.length === 0) throw Error('Size is empty');
+
+        if (!this.productIsValid()) return;
 
         return `${this.name} (${this.price}), ${this.size}`;
     }
