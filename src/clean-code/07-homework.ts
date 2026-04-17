de3265c585f5799e19a51fab018a41da7a88b38e
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-17 15:33:05 -0600

    fix: Corrección de la actividad aplicando principio de responsabilidad unica

--- src/clean-code/07-homework.ts
+++ src/clean-code/07-homework.ts
@@ -1,43 +1,60 @@
-(()=>{
+(() => {
 
     //* Aplicar el principio de responsabilidad única
     //* Priorizar la composición frente a la herencia
 
-    type HtmlType = 'input'|'select'|'textarea'|'radio';
+    type HtmlType = 'input' | 'select' | 'textarea' | 'radio';
 
     class HtmlElement {
         constructor(
             public id: string,
             public type: HtmlType,
-        ) {}
-    }
-
-    class InputAttributes extends HtmlElement {
-        constructor(
-            public value: string,
-            public placeholder: string,
-            id: string,
         ) {
-            super(id, 'input');
         }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
+    class InputAttributes {
+        constructor(
+            public placeholder: string,
+            public value: string,
+        ) {
+        }
+    }
+
+    class InputEvents {
+        constructor() {
         }
 
-        setFocus() {};
-        getValue() {};
-        isActive() {};
-        removeValue() {};
+        setFocus() {
+        };
+
+        getValue() {
+        };
+
+        isActive() {
+        };
+
+        removeValue() {
+        };
+    }
+
+    class InputElement {
+        public htmlElement: HtmlElement;
+        public inputAttributes: InputAttributes
+        public events: InputEvents;
+
+        constructor(value: string, placeholder: string, id: string) {
+            this.htmlElement = new HtmlElement(id, 'input');
+            this.inputAttributes = new InputAttributes(placeholder, value);
+            this.events = new InputEvents();
+        }
     }
 
 
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
 
-    console.log({ nameField });
+    console.log({nameField});
 
 })()
