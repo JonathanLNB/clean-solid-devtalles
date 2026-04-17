89354b13cfdfa65caf4d608c0a730bdf6707f676
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-17 11:19:10 -0600

    chore: Proyecto base

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <h1>Clean SOLID</h1>
+  <h2>Revisa la consola de JavaScript</h2>
+</section>`
+
