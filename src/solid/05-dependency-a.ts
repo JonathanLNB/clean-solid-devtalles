787015b58de77dea341bbf3d8a1c0f856383b090
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-20 14:16:15 -0600

    fix: Se corrige el ejemplo aplicado al principio de inversión de dependencias con substitución de Liskov

--- src/solid/05-dependency-a.ts
+++ src/solid/05-dependency-a.ts
@@ -1,14 +1,18 @@
-import { PostService } from './05-dependency-b';
+import {PostService} from './05-dependency-b';
+import {FetchProvider, JsonDataBaseService, LocalDataBaseService, WebPostProvider} from "./05-dependency-c.ts";
 
 
 // Main
 (async () => {
 
-    const postService = new PostService();
+    //const provider = new LocalDataBaseService();
+    const fetchProvider = new FetchProvider();
+    const provider = new WebPostProvider(fetchProvider);
+    const postService = new PostService(provider);
 
     const posts = await postService.getPosts();
 
-    console.log({ posts })
+    console.log({posts})
 
 
 })();
--- src/solid/05-dependency-c.ts
+++ src/solid/05-dependency-c.ts
@@ -1,9 +1,30 @@
+import localPosts from '../data/local-database.json'
+import type {Post} from "./05-dependency-b.ts";
 
-export class LocalDataBaseService {
 
-    constructor() {}
+export abstract class PostProvider {
+    abstract getPosts(): Promise<Post[]>;
+}
 
-    async getFakePosts() {
+export interface HttpProvider {
+    get(url: string): Promise<any>;
+}
+
+export class FetchProvider implements HttpProvider {
+    async get(url: string): Promise<any> {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return data;
+    }
+
+}
+
+export class LocalDataBaseService implements PostProvider {
+
+    constructor() {
+    }
+
+    getPosts(): Promise<Post[]> {
         return [
             {
                 'userId': 1,
@@ -16,7 +37,26 @@ export class LocalDataBaseService {
                 'id': 2,
                 'title': 'qui est esse',
                 'body': 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla'
-            }]
+            }];
+    }
+
+}
+
+export class JsonDataBaseService implements PostProvider {
+    async getPosts() {
+        return localPosts;
+    }
+}
+
+export class WebPostProvider implements PostProvider {
+
+    constructor(
+        private http: HttpProvider,
+    ) {
+    }
+
+    getPosts(): Promise<Post[]> {
+        return this.http.get('https://jsonplaceholder.typicode.com/posts');
     }
 
 }
